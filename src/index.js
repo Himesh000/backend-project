60e64439f743f467021bc2679e0f80a6ff60ffe5
Himesh Verma <vermahimesh001@gmail.com>
2025-06-06 18:50:47 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection Failed!!", err);
+  });
